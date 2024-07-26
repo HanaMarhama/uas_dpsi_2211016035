051c9db3426f4a39c74da43d18e9977fc1fc6b46
HanaMarhama <marhamahasana@gmail.com>
2024-07-27 01:38:51 +0700

    update config

--- config.js
+++ config.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 
 module.exports = {
   development: {
+    host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
     dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    dialectModule: require("mysql2"),
+    benchmark: true,
   },
 };
